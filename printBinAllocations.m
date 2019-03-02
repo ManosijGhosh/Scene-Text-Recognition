79e5d935dad9927228a211f97d3f0eff1fa25bd6
IndraTheZeus <indranarayandutta@gmail.com>
2019-03-02 19:13:22 +0530

    Binning Files 02-03-2019

---
+++ printBinAllocations.m
@@ -0,0 +1,24 @@
+function [X] = printBinAllocations(BinSizes,MAX_DISTANCE,NumBinImages)
+
+   X = zeros(NumBinImages,4);
+   %[ imageNo Size start end ]
+   q_offset = 0;
+   for i = 1:numel(BinSizes)
+      main_offset = ceil(MAX_DISTANCE/BinSizes(i));
+      k = ceil((BinSizes(i)/2)) -1;
+      start = 0;
+      for img_no = (q_offset+1):(q_offset+main_offset)
+          X(img_no,:) = [img_no BinSizes(i) start (start+BinSizes(i)-1)];
+          start = start + BinSizes(i);
+      end
+       start = k+1;
+      for img_no = (q_offset+main_offset+1):(q_offset+2*main_offset-1)
+           X(img_no,:) = [img_no BinSizes(i) start (start+BinSizes(i))];
+          start = start + BinSizes(i);          
+      end    
+       
+      q_offset = q_offset + 2*main_offset-1; 
+   end
+
+
+end
