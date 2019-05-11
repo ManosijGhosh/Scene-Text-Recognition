7ddca268cdb1a8f121d347a9c06e2a199267e6cd
IndraTheZeus <indranarayandutta@gmail.com>
2019-05-11 21:08:32 +0530

    10 images extraction done

--- datacreate.m
+++ datacreate.m
@@ -4,9 +4,11 @@ function [data] = datacreate(n,dimension,scale,lb,ub)
 
 rng('shuffle');
 data=zeros(n,dimension);
+scaling_matrix = zeros(n,dimension,2);
 ub = ub/scale;
 for i=1:n
     temp=rand(1,dimension);
+    scaling_matrix(i,:,:) = rand(1,dimension,2);
 %     temp=temp*(ub-lb)+lb;
     data(i,:)=temp;
 end
@@ -24,7 +26,7 @@ catch_all = [ 1 1 1 1 1 1 0 1 1 1 0 1 0 1 0 1 1 1 1 1 0 1 0 1 0 1 0 1 1 1 0 1 0 
 % %     data(:,~ideal) = max(0.25,data(:,~ideal));
 %data(1,:) = catch_all;
 for i=1:n
-    data(i,:) = (max_k*data(i,:) + min_dev).*ideal;
+    data(i,:) = (((max_k*data(i,:)).*scaling_matrix(i,:,1)) + (min_dev*scaling_matrix(i,:,2))).*ideal;
 end
 % % %     data(1,:) = ideal;
 % % data(:,16:19) = 0.25;
